4f53fac2ba640053654d5774702ca9368a027dbd
Taras Pinkas <pinkas.taras@gmail.com>
2018-04-20 02:42:52 +0300

    Cleanup test ethernet

--- src/test/test_ether/test_ether.ino
+++ src/test/test_ether/test_ether.ino
@@ -1,74 +1,94 @@
-// Demo using DHCP and DNS to perform a web client request.
-// 2011-06-08 <jc@wippler.nl> http://opensource.org/licenses/mit-license.php
-
 #include <EtherCard.h>
-#define Trig 5
-#define Echo 6
-#define ledPin 13
+#include <string.h>
+
+#define DATA_LEN 40
+#define TIMER_DELAY 5000 // msecs
 
 // ethernet interface mac address, must be unique on the LAN
 static byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
 
 byte Ethernet::buffer[700];
-static uint32_t timer;
+static unsigned long timer = 0;
 
 const char website[] PROGMEM = "www.tobo.com.ua";
 
 // called when the client request is complete
-static void my_callback (byte status, word off, word len) {
-  Serial.println(">>>");
-  Ethernet::buffer[off+300] = 0;
-  Serial.print((const char*) Ethernet::buffer + off);
-  Serial.println("...");
+static void my_callback (byte status, word off, word len)
+{
+	Serial.println(">>>");
+
+	Ethernet::buffer[off+300] = 0;
+
+	Serial.print((const char*) Ethernet::buffer + off);
+	Serial.println("...");
 }
 
-void setup () {
-  pinMode(Trig, OUTPUT); //инициируем как выход 
-  pinMode(Echo, INPUT); //инициируем как вход 
-  pinMode(ledPin, OUTPUT); 
-  Serial.begin(57600);
-  Serial.println(F("\n[webClient]"));
+bool setup_eth(const byte *mac, const char *site)
+{
+	if (ether.begin(sizeof Ethernet::buffer, mac) == 0)
+	{
+		Serial.println(F("Failed to access Ethernet controller"));
+		return false;
+	}
 
-  if (ether.begin(sizeof Ethernet::buffer, mymac) == 0) 
-    Serial.println(F("Failed to access Ethernet controller"));
-  if (!ether.dhcpSetup())
-    Serial.println(F("DHCP failed"));
+	if (!ether.dhcpSetup())
+	{
+		Serial.println(F("DHCP failed"));
+		return false;
+	}
 
-  ether.printIp("IP:  ", ether.myip);
-  ether.printIp("GW:  ", ether.gwip);  
-  ether.printIp("DNS: ", ether.dnsip);  
+	ether.printIp("IP:  ", ether.myip);
+	ether.printIp("GW:  ", ether.gwip);
+	ether.printIp("DNS: ", ether.dnsip);
 
-  if (!ether.dnsLookup(website))
-    Serial.println("DNS failed");
-    
-  ether.printIp("SRV: ", ether.hisip);
+	if (!ether.dnsLookup(site))
+	{
+		Serial.println("DNS failed");
+		return false;
+	}
+
+	ether.printIp("SRV: ", ether.hisip);
+
+	return true;
 }
-unsigned int impulseTime=0; 
-unsigned int distance_sm=0; 
 
-static int dist(){
-  digitalWrite(Trig, HIGH); 
-  /* Подаем импульс на вход trig дальномера */
-  delayMicroseconds(10); // равный 10 микросекундам 
-  digitalWrite(Trig, LOW); // Отключаем 
-  impulseTime=pulseIn(Echo, HIGH); // Замеряем длину импульса 
-  return (impulseTime/58); // Пересчитываем в сантиметры 
-  }
 
-void loop () {
-  ether.packetLoop(ether.packetReceive());
-  
-  if (millis() > timer) {
-    timer = millis() + 5000;
-    Serial.println();
-    Serial.print("<<< REQ ");
-    String dataToBdSt = "?one=";
-  dataToBdSt += 1;
-  dataToBdSt += "&two=";
-  dataToBdSt += dist();
-  char dataToBdCh[40];
-  dataToBdSt.toCharArray(dataToBdCh, 40); 
- 
-    ether.browseUrl(PSTR("/test.php"), dataToBdCh, website, my_callback);
-  }
+
+bool eth_func(const char *site)
+{
+	char dataToBdCh[DATA_LEN] = { 0 };
+
+	if (snprintf(dataToBdCh, DATA_LEN, "?%s%2d&%s%2d&%s%2d&%s%2d",
+				"moisture=", 1, "pump=", 3, "temp=", 4, "level=", 6) < 0)
+	{
+		Serial.println("Failed to prepare the request");
+		return false;
+	}
+
+	ether.browseUrl(PSTR("/test.php"), dataToBdCh, site, my_callback);
+	return true;
+}
+
+void setup ()
+{
+
+	Serial.begin(57600);
+	Serial.println(F("\n[webClient]"));
+
+	if (!setup_eth(mymac, website))
+		Serial.println("Failed to setup Ethernet module");
+
+}
+
+void loop ()
+{
+	ether.packetLoop(ether.packetReceive());
+
+	if (millis() > timer)
+	{
+		timer = millis() + TIMER_DELAY;
+		Serial.println();
+		Serial.print("<<< REQ ");
+		eth_func(website);
+	}
 }
